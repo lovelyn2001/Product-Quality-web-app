a9ad0e8dc0eddf88988c9d0920f821e6bb969c96
lovelyn2001 <lovelynikwuka2001@gmail.com>
2024-10-28 18:34:47 +0100

    second commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 const PREDEFINED_PASSWORD = 'Mouau';
 
 app.set('view engine', 'ejs');
@@ -96,6 +96,5 @@ app.post('/dashboard', async (req, res) => {
   res.render('dashboard', { alert });
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+// Start server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
